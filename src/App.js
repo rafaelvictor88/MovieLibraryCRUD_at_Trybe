82154c73904106810916ea3b0616f24726f20afe
Rafael Victor <rafavictorgs@gmail.com>
2021-08-02 17:57:39 -0300

    Resolve o requisito 1 com auxilio do Vitor Ferreira

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, EditMovie, MovieDetails, NewMovie, NotFound } from './pages';
 
 class App extends React.Component {
   render() {
     return (
-      <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
